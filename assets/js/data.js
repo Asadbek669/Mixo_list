43d84483feaa8105a4018546e05958c56e2087d8
Asadbek669 <akbarovasadbek777@gmail.com>
2026-02-05 17:39:51 +0300

    Add files via upload

---
+++ assets/js/data.js
@@ -0,0 +1,17 @@
+const koreaDramas = [
+  {
+    title: "Squid Game",
+    poster: "https://cdn.fastora.uz/Series/SquidGame/poster.webp",
+    link: "https://t.me/minxo_tv/1"
+  },
+  {
+    title: "Crash Landing On You",
+    poster: "https://cdn.fastora.uz/Series/CrashLandingOnYou/poster.webp",
+    link: "https://t.me/minxo_tv/2"
+  },
+  {
+    title: "Itaewon Class",
+    poster: "https://cdn.fastora.uz/Series/ItaewonClass/poster.webp",
+    link: "https://t.me/minxo_tv/3"
+  }
+];
