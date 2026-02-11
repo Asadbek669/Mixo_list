30b2afe51243c9cfc07646953a3646f9f53c886f
Asadbek669 <akbarovasadbek777@gmail.com>
2026-02-12 01:09:04 +0300

    Update data.js: Zolushka va to'rt ritsar

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,5 +1,10 @@
 const koreaDramas = [
   {
+    "title": "Zolushka va to'rt ritsar",
+    "poster": "https://cdn.fastora.uz/minxo_tv/zolushka-va-tort-ritsar.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/8464"
+  },
+  {
     "title": "Taqiqlangan nikoh",
     "poster": "https://cdn.fastora.uz/minxo_tv/taqiqlangan-nikoh.jpg",
     "link": "https://t.me/Minxotv_Arxiv/7878"
