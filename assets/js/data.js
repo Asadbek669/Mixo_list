2f204c387f595b3f1c823976a055b3e4a54262d1
Asadbek669 <akbarovasadbek777@gmail.com>
2026-02-12 02:01:06 +0300

    Update data.js

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,158 +1,8 @@
 const koreaDramas = [
   {
-    "title": "Hushyor",
-    "poster": "https://cdn.fastora.uz/minxo_tv/hushyor.jpg",
-    "link": "https://t.me/Minxotv_Arxiv/6041"
-  },
-  {
-    "title": "Kapalak sevgi shakli",
-    "poster": "https://cdn.fastora.uz/minxo_tv/kapalak-sevgi-shakli.jpg",
-    "link": "https://t.me/Minxotv_Arxiv/77"
-  },
-  {
-    "title": "O'tkir pichoq",
-    "poster": "https://cdn.fastora.uz/minxo_tv/otkir-pichoq.jpg",
-    "link": "https://t.me/Minxotv_Arxiv/5021"
-  },
-  {
-    "title": "Sarmast ishq",
-    "poster": "https://cdn.fastora.uz/minxo_tv/sarmast-ishq.jpg",
-    "link": "https://t.me/Minxotv_Arxiv/6040"
-  },
-  {
-    "title": "Salmariga hush kebsiz",
-    "poster": "https://cdn.fastora.uz/minxo_tv/salmariga-hush-kebsiz.jpg",
-    "link": "https://t.me/Minxotv_Arxiv/5153"
-  },
-  {
-    "title": "Qora olov ajdari",
-    "poster": "https://cdn.fastora.uz/minxo_tv/qora-olov-ajdari.jpg",
-    "link": "https://t.me/Minxotv_Arxiv/4924"
-  },
-  {
-    "title": "Yoshlik bahori",
-    "poster": "https://cdn.fastora.uz/minxo_tv/yoshlik-bahori.jpg",
-    "link": "https://t.me/Minxotv_Arxiv/6297"
-  },
-  {
-    "title": "O'smir stajyor",
-    "poster": "https://cdn.fastora.uz/minxo_tv/osmir-stajyor.jpg",
-    "link": "https://t.me/Minxotv_Arxiv/7610"
-  },
-  {
-    "title": "Og'ir atletika parisi",
-    "poster": "https://cdn.fastora.uz/minxo_tv/ogir-atletika-parisi.jpg",
-    "link": "https://t.me/Minxotv_Arxiv/7328"
-  },
-  {
-    "title": "Qadrli hong rang",
-    "poster": "https://cdn.fastora.uz/minxo_tv/qadrli-hong-rang.jpg",
-    "link": "https://t.me/Minxotv_Arxiv/7445"
-  },
-  {
-    "title": "Zolimning oshpazi",
-    "poster": "https://cdn.fastora.uz/minxo_tv/zolimning-oshpazi.jpg",
-    "link": "https://t.me/Minxotv_Arxiv/7771"
-  },
-  {
-    "title": "Mening partadoshim",
-    "poster": "https://cdn.fastora.uz/minxo_tv/mening-partadoshim.jpg",
-    "link": "https://t.me/Minxotv_Arxiv/2804"
-  },
-  {
-    "title": "19 Hayotimda Korishguncha",
-    "poster": "https://cdn.fastora.uz/minxo_tv/19-hayotimda-korishguncha.jpg",
-    "link": "https://t.me/Minxotv_Arxiv/7594"
-  },
-  {
-    "title": "Kechani o'g'irladim",
-    "poster": "https://cdn.fastora.uz/minxo_tv/kechani-ogirladim.jpg",
-    "link": "https://t.me/Minxotv_Arxiv/7677"
-  },
-  {
-    "title": "Bo'ron",
-    "poster": "https://cdn.fastora.uz/minxo_tv/boron.jpg",
-    "link": "https://t.me/Minxotv_Arxiv/7811"
-  },
-  {
-    "title": "Yonimda bo'lganingda",
-    "poster": "https://cdn.fastora.uz/minxo_tv/yonimda-bolganingda.jpg",
-    "link": "https://t.me/Minxotv_Arxiv/8088"
-  },
-  {
-    "title": "Ruh barmoqlari",
-    "poster": "https://cdn.fastora.uz/minxo_tv/ruh-barmoqlari.jpg",
-    "link": "https://t.me/Minxotv_Arxiv/8027"
-  },
-  {
-    "title": "Yozilmagan Seul Hikoyasi",
-    "poster": "https://cdn.fastora.uz/minxo_tv/yozilmagan-seul-hikoyasi.jpg",
-    "link": "https://t.me/Minxotv_Arxiv/7671"
-  },
-  {
-    "title": "19 qavat",
-    "poster": "https://cdn.fastora.uz/minxo_tv/19-qavat.jpg",
-    "link": "https://t.me/Minxotv_Arxiv/8157"
-  },
-  {
-    "title": "Bosa olmaslik kerak edi",
-    "poster": "https://cdn.fastora.uz/minxo_tv/bosa-olmaslik-kerak-edi.jpg",
-    "link": "https://t.me/Minxotv_Arxiv/8120"
-  },
-  {
-    "title": "Trigger",
-    "poster": "https://cdn.fastora.uz/minxo_tv/trigger.jpg",
-    "link": "https://t.me/Minxotv_Arxiv/8206"
-  },
-  {
-    "title": "Yovuz ruh",
-    "poster": "https://cdn.fastora.uz/minxo_tv/yovuz-ruh.jpg",
-    "link": "https://t.me/Minxotv_Arxiv/8234"
-  },
-  {
-    "title": "Quyosh nurini o'lchami",
-    "poster": "https://cdn.fastora.uz/minxo_tv/quyosh-nurini-olchami.jpg",
-    "link": "https://t.me/Minxotv_Arxiv/8387"
-  },
-  {
-    "title": "Idolning advokati",
-    "poster": "https://cdn.fastora.uz/minxo_tv/idolning-advokati.jpg",
-    "link": "https://t.me/Minxotv_Arxiv/8383"
-  },
-  {
-    "title": "9 jumboq",
-    "poster": "https://cdn.fastora.uz/minxo_tv/9-jumboq.jpg",
-    "link": "https://t.me/Minxotv_Arxiv/7895"
-  },
-  {
-    "title": "Bahoriy hislar",
-    "poster": "https://cdn.fastora.uz/minxo_tv/bahoriy-hislar.jpg",
-    "link": "https://t.me/Minxotv_Arxiv/8386"
-  },
-  {
-    "title": "Oy Ogʻushidagi ishq",
-    "poster": "https://cdn.fastora.uz/minxo_tv/oy-ogʻushidagi-ishq.jpg",
-    "link": "https://t.me/Minxotv_Arxiv/8138"
-  },
-  {
-    "title": "Men bugundan insonman",
-    "poster": "https://cdn.fastora.uz/minxo_tv/men-bugundan-insonman.jpg",
-    "link": "https://t.me/Minxotv_Arxiv/8430"
-  },
-  {
-    "title": "Bizning olam",
-    "poster": "https://cdn.fastora.uz/minxo_tv/bizning-olam.jpg",
-    "link": "https://t.me/Minxotv_Arxiv/8497"
-  },
-  {
-    "title": "Zolushka va to'rt ritsar",
-    "poster": "https://cdn.fastora.uz/minxo_tv/zolushka-va-tort-ritsar.jpg",
-    "link": "https://t.me/Minxotv_Arxiv/8464"
-  },
-  {
-    "title": "Taqiqlangan nikoh",
-    "poster": "https://cdn.fastora.uz/minxo_tv/taqiqlangan-nikoh.jpg",
-    "link": "https://t.me/Minxotv_Arxiv/7878"
+    "title": "25 & 21",
+    "poster": "https://cdn.fastora.uz/minxo_tv/25-21.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/7731"
   },
   {
     "title": "Qulayotgan yulduz",
@@ -160,8 +10,158 @@ const koreaDramas = [
     "link": "https://t.me/Minxotv_Arxiv/7672"
   },
   {
-    "title": "25 & 21",
-    "poster": "https://cdn.fastora.uz/minxo_tv/25-21.jpg",
-    "link": "https://t.me/Minxotv_Arxiv/7731"
+    "title": "Taqiqlangan nikoh",
+    "poster": "https://cdn.fastora.uz/minxo_tv/taqiqlangan-nikoh.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/7878"
+  },
+  {
+    "title": "Zolushka va to'rt ritsar",
+    "poster": "https://cdn.fastora.uz/minxo_tv/zolushka-va-tort-ritsar.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/8464"
+  },
+  {
+    "title": "Bizning olam",
+    "poster": "https://cdn.fastora.uz/minxo_tv/bizning-olam.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/8497"
+  },
+  {
+    "title": "Men bugundan insonman",
+    "poster": "https://cdn.fastora.uz/minxo_tv/men-bugundan-insonman.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/8430"
+  },
+  {
+    "title": "Oy Ogʻushidagi ishq",
+    "poster": "https://cdn.fastora.uz/minxo_tv/oy-ogʻushidagi-ishq.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/8138"
+  },
+  {
+    "title": "Bahoriy hislar",
+    "poster": "https://cdn.fastora.uz/minxo_tv/bahoriy-hislar.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/8386"
+  },
+  {
+    "title": "9 jumboq",
+    "poster": "https://cdn.fastora.uz/minxo_tv/9-jumboq.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/7895"
+  },
+  {
+    "title": "Idolning advokati",
+    "poster": "https://cdn.fastora.uz/minxo_tv/idolning-advokati.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/8383"
+  },
+  {
+    "title": "Quyosh nurini o'lchami",
+    "poster": "https://cdn.fastora.uz/minxo_tv/quyosh-nurini-olchami.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/8387"
+  },
+  {
+    "title": "Yovuz ruh",
+    "poster": "https://cdn.fastora.uz/minxo_tv/yovuz-ruh.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/8234"
+  },
+  {
+    "title": "Trigger",
+    "poster": "https://cdn.fastora.uz/minxo_tv/trigger.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/8206"
+  },
+  {
+    "title": "Bosa olmaslik kerak edi",
+    "poster": "https://cdn.fastora.uz/minxo_tv/bosa-olmaslik-kerak-edi.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/8120"
+  },
+  {
+    "title": "19 qavat",
+    "poster": "https://cdn.fastora.uz/minxo_tv/19-qavat.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/8157"
+  },
+  {
+    "title": "Yozilmagan Seul Hikoyasi",
+    "poster": "https://cdn.fastora.uz/minxo_tv/yozilmagan-seul-hikoyasi.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/7671"
+  },
+  {
+    "title": "Ruh barmoqlari",
+    "poster": "https://cdn.fastora.uz/minxo_tv/ruh-barmoqlari.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/8027"
+  },
+  {
+    "title": "Yonimda bo'lganingda",
+    "poster": "https://cdn.fastora.uz/minxo_tv/yonimda-bolganingda.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/8088"
+  },
+  {
+    "title": "Bo'ron",
+    "poster": "https://cdn.fastora.uz/minxo_tv/boron.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/7811"
+  },
+  {
+    "title": "Kechani o'g'irladim",
+    "poster": "https://cdn.fastora.uz/minxo_tv/kechani-ogirladim.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/7677"
+  },
+  {
+    "title": "19 Hayotimda Korishguncha",
+    "poster": "https://cdn.fastora.uz/minxo_tv/19-hayotimda-korishguncha.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/7594"
+  },
+  {
+    "title": "Mening partadoshim",
+    "poster": "https://cdn.fastora.uz/minxo_tv/mening-partadoshim.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/2804"
+  },
+  {
+    "title": "Zolimning oshpazi",
+    "poster": "https://cdn.fastora.uz/minxo_tv/zolimning-oshpazi.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/7771"
+  },
+  {
+    "title": "Qadrli hong rang",
+    "poster": "https://cdn.fastora.uz/minxo_tv/qadrli-hong-rang.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/7445"
+  },
+  {
+    "title": "Og'ir atletika parisi",
+    "poster": "https://cdn.fastora.uz/minxo_tv/ogir-atletika-parisi.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/7328"
+  },
+  {
+    "title": "O'smir stajyor",
+    "poster": "https://cdn.fastora.uz/minxo_tv/osmir-stajyor.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/7610"
+  },
+  {
+    "title": "Yoshlik bahori",
+    "poster": "https://cdn.fastora.uz/minxo_tv/yoshlik-bahori.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/6297"
+  },
+  {
+    "title": "Qora olov ajdari",
+    "poster": "https://cdn.fastora.uz/minxo_tv/qora-olov-ajdari.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/4924"
+  },
+  {
+    "title": "Salmariga hush kebsiz",
+    "poster": "https://cdn.fastora.uz/minxo_tv/salmariga-hush-kebsiz.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/5153"
+  },
+  {
+    "title": "Sarmast ishq",
+    "poster": "https://cdn.fastora.uz/minxo_tv/sarmast-ishq.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/6040"
+  },
+  {
+    "title": "O'tkir pichoq",
+    "poster": "https://cdn.fastora.uz/minxo_tv/otkir-pichoq.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/5021"
+  },
+  {
+    "title": "Kapalak sevgi shakli",
+    "poster": "https://cdn.fastora.uz/minxo_tv/kapalak-sevgi-shakli.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/77"
+  },
+  {
+    "title": "Hushyor",
+    "poster": "https://cdn.fastora.uz/minxo_tv/hushyor.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/6041"
   }
 ];
