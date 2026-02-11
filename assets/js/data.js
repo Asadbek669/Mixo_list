a8e27d620a33a6b53deae39c9b21fbf6cde883a6
Asadbek669 <akbarovasadbek777@gmail.com>
2026-02-12 01:07:40 +0300

    Update data.js: Taqiqlangan nikoh

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,5 +1,10 @@
 const koreaDramas = [
   {
+    "title": "Taqiqlangan nikoh",
+    "poster": "https://cdn.fastora.uz/minxo_tv/taqiqlangan-nikoh.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/7878"
+  },
+  {
     "title": "Qulayotgan yulduz",
     "poster": "https://cdn.fastora.uz/minxo_tv/qulayotgan-yulduz.jpg",
     "link": "https://t.me/Minxotv_Arxiv/7672"
