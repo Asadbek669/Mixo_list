9289d06f31906e5b5e1acc63c6a31b9e734aa80a
Asadbek669 <akbarovasadbek777@gmail.com>
2026-02-12 00:27:52 +0300

    Update data.js: Qulayotgan yulduz

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,5 +1,10 @@
 const koreaDramas = [
   {
+    "title": "Qulayotgan yulduz",
+    "poster": "https://cdn.fastora.uz/minxo_tv/qulayotgan-yulduz.jpg",
+    "link": "https://t.me/Minxotv_Arxiv/7672"
+  },
+  {
     "title": "25 & 21",
     "poster": "https://cdn.fastora.uz/minxo_tv/25-21.jpg",
     "link": "https://t.me/Minxotv_Arxiv/7731"
